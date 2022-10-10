a8ded4f14a13865bd0650b732e8096a17f53c4fc
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2022-10-10 23:02:47 +0600

    added top header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../../images/logo.png';
 
 const Header = () => {
   return (
-    <div>
-      <h1>This is header</h1>
+    <div className='flex justify-between items-center w-10/12 mx-auto bg-slate-100 py-1 px-2 mt-2 rounded-lg'>
+      <div className='w-12'>
+        <Link className='flex items-center'>
+          <img src={Logo} alt="Brand Logo" />
+          <h1 className='text-3xl font-bold text-blue-500'>QuizKit</h1>
+        </Link>
+      </div>
+      <div className='text-md'>
+        <Link className='mx-2 px-3 py-2 text-blue-500 font-medium hover:bg-blue-100 rounded-md duration-300 ease-in-out	' to='/home'>Home</Link>
+        <Link className='mx-2 px-3 py-2 text-blue-500 font-medium hover:bg-blue-100 rounded-md duration-300 ease-in-out	' to='/topics'>Topics</Link>
+        <Link className='mx-2 px-3 py-2 text-blue-500 font-medium hover:bg-blue-100 rounded-md duration-300 ease-in-out	' to='/staticts'>Staticts</Link>
+        <Link className='mx-2 px-3 py-2 text-blue-500 font-medium hover:bg-blue-100 rounded-md duration-300 ease-in-out	' to='/blog'>Blog</Link>
+      </div>
     </div>
   );
 };
