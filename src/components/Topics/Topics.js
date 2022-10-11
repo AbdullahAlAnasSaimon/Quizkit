4f7330d4d044010e69d136adfb77c257598e2227
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2022-10-11 14:16:59 +0600

    compoleted quizes section

--- src/components/Topics/Topics.js
+++ src/components/Topics/Topics.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 
 const Topics = ({ topics }) => {
   const quizes = useLoaderData();
@@ -8,7 +8,7 @@ const Topics = ({ topics }) => {
     return (
       <div>
         <h1 className='text-4xl font-bold text-center my-5'>Start Quizzes</h1>
-        <div>
+        <div className='grid grid-cols-2 w-11/12 mx-auto'>
           {
             topics.map(topic => <Topic key={topic.id} topic={topic}></Topic>)
           }
@@ -18,7 +18,7 @@ const Topics = ({ topics }) => {
   }
   else{
     return (
-      <div>
+      <div className='w-10/12 mx-auto'>
         <div className='grid grid-cols-2'>
           {
             quizes.data.map(topic => <Topic key={topic.id} topic={topic}></Topic>)
@@ -33,14 +33,14 @@ const Topics = ({ topics }) => {
 const Topic = ({ topic }) => {
   const { id, name, logo, total } = topic;
   return (
-    <div className='flex'>
+    <div className='flex justify-between m-5 p-4 bg-blue-100 shadow-lg rounded-lg'>
       <div>
-        <h2>{name}</h2>
-        <p>{total}</p>
-        <button>Start Quiz</button>
+        <h2 className='text-3xl font-semibold my-2'>{name}</h2>
+        <p className='my-2'>Total Quiz: {total}</p>
+        <Link className='py-1 px-3 bg-blue-400 my-3 rounded-md hover:bg-blue-500'>Start Quiz &rarr;</Link>
       </div>
-      <div>
-        <img src={logo} alt="" />
+      <div className='w-3/12 bg-slate-800 rounded-lg'>
+        <img className='rounded-lg' src={logo} alt="" />
       </div>
     </div>
   );
