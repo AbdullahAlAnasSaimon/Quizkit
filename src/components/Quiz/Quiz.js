52ea56f002618e658fde4388f6782f4cf964abd4
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2022-10-11 17:20:20 +0600

    added toast

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,9 +1,10 @@
-import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const Quiz = () => {
   const quizkit = useLoaderData();
-
   const { name, total, questions } = quizkit.data;
 
   return (
@@ -18,36 +19,78 @@ const Quiz = () => {
           ></Question>)
         }
       </div>
+      <ToastContainer/>
     </div>
   );
 };
 
 const Question = ({ questionObj }) => {
-  console.log(questionObj);
-  const { id, correctAnswer, options, question } = questionObj;
-
+  // const [ans, setAns] = useState('');
+  // console.log(questionObj);
+  const { correctAnswer, options, question } = questionObj;
+  // setAns(correctAnswer);
   let questions = question.replace(/<\/?[^>]+(>|$)/g, "");
 
+
+
+  
   return (
     <div>
-      <div className='my-10 border-2 border-blue-300 py-4 pb-8'>
+      <div className='my-10 border-2 border-blue-300 rounded-lg py-4 pb-8'>
         <div className='mb-8 mt-4 mx-8 text-2xl font-semibold'>{questions}</div>
         {
           options.map((option, index) => <Option
             key={index}
             option={option}
+            correctAnswer={correctAnswer}
           ></Option>)
         }
       </div>
     </div>
   );
+
+
 };
 
-const Option = ({ option }) => {
+
+
+const Option = ({ option, correctAnswer }) => {
+
+  const handleButtonClick = (event) =>{
+    const questionInnerText = event.target.innerText;
+    if(questionInnerText === correctAnswer){
+      event.target.classList.add('bg-green-300');
+      const notify = () => toast.success('Answer is Correct!', {
+        position: "bottom-center",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        });
+        notify();
+    }else{
+      event.target.classList.add('bg-red-300');
+      const notify = () => toast.error('Wrong Answer!', {
+        position: "bottom-center",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        });
+        notify();
+    }
+  }
+
   return (
     <div className='my-2 rounded-md overflow-hidden'>
-      <button className='mx-8 p-2 bg-slate-100 w-11/12 rounded-md'>
-        <div className='text-left'>{option}</div>
+      <button id='quiz-btn' className='text-left mx-8 p-2 bg-slate-100 w-11/12 rounded-md' onClick={handleButtonClick}>
+        {option}
       </button>
     </div>
   );
