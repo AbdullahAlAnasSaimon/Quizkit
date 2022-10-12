580865841d48ee954cb38272e34605dd5e8e9be4
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2022-10-12 21:15:15 +0600

    added count and button disabled functionality

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -7,7 +7,17 @@ import 'react-toastify/dist/ReactToastify.css';
 
 
 const Quiz = () => {
-  window.scrollTo(0, 0);
+// set state for update answer count
+  const [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(0);
+
+  const trueValue = (value) =>{
+    setCount(count + value);
+  }
+  const falseValue = (value) =>{
+    setCount2(count2 + value);
+  }
+// receiving data from api
   const quizkit = useLoaderData();
   const { name, total, questions } = quizkit.data;
 
@@ -15,11 +25,17 @@ const Quiz = () => {
     <div>
       <h2 className='text-4xl text-center font-semibold my-5'>{name} Quizes</h2>
       <p className='text-center my-5'>Total Quiz : {total}</p>
+      <div className='text-center border-2 bg-blue-200 border-blue-300 w-8/12 mx-auto p-2 rounded-lg'>
+        <p className='text-lg font-semibold my-2'>Correct Answer: {count}</p>
+        <p className='text-lg font-semibold my-2'>Wrong Answer: {count2}</p>
+      </div>
       <div className='w-11/12 md:w-8/12 mx-auto'>
         {
           questions.map(question => <Question
             key={question.id}
             questionObj={question}
+            trueValue={trueValue}
+            falseValue={falseValue}
           ></Question>)
         }
       </div>
@@ -28,15 +44,16 @@ const Quiz = () => {
   );
 };
 
-
-const Question = ({ questionObj }) => {
+// question component
+const Question = ({ questionObj, trueValue, falseValue }) => {
+  // state for toggoler
   const [show, setShow] = useState(true);
-  const { correctAnswer, options, question } = questionObj;
+  const { id, correctAnswer, options, question } = questionObj;
 
   const toggleShow = () => {
     setShow(!show);
   }
-
+// remove p tag from questions
   let questions = question.replace(/<\/?[^>]+(>|$)/g, "");
 
   return (
@@ -44,17 +61,22 @@ const Question = ({ questionObj }) => {
       <div className='my-10 border-2 border-blue-300 rounded-lg py-4'>
         <div className='flex justify-between items-center w-11/12 mx-auto mt-4 mb-6'>
           <div className='text-lg md:text-2xl font-semibold w-11/12'>{questions}</div>
+          {/* <div>{count}</div> */}
           <button onClick={toggleShow}>
             <FontAwesomeIcon icon={show ? faEye : faEyeSlash} className="p-2 bg-gray-100 hover:bg-slate-200 rounded-full"></FontAwesomeIcon>
           </button>
         </div>
-        {
-          options.map((option, index) => <Option
-            key={index}
-            option={option}
-            correctAnswer={correctAnswer}
-          ></Option>)
-        }
+        <div id={id}>
+          {
+            options.map((option, index) => <Option
+              key={index}
+              option={option}
+              correctAnswer={correctAnswer}
+              trueValue={trueValue}
+              falseValue={falseValue}
+            ></Option>)
+          }
+        </div>
         <div className={`mx-8 mt-8 ${show ? show : 'p-4'} bg-green-300 rounded-lg`}>
           {show ? show : <span className='font-semibold'>Correct Answer : </span>}
           {show ? show : correctAnswer}
@@ -64,15 +86,23 @@ const Question = ({ questionObj }) => {
   );
 };
 
-
-
-const Option = ({ option, correctAnswer }) => {
+// option component for show the answer option
+const Option = ({ option, correctAnswer, trueValue, falseValue }) => {
 
   const handleButtonClick = (event) => {
 
     const questionInnerText = event.target.innerText.trim();
     const correctAnswer2 = correctAnswer.replace(/\s+/g, ' ').trim();
+    
+    const htg = event.target.innerText
+    event.target.parentNode.childNodes.forEach(node => {
+      if(htg !== node.innerText){
+        node.setAttribute("disabled", true);
+        node.classList.add('bg-gray-200', 'text-gray-400');
+      }
+    })
 
+    // implement true condition
     if (questionInnerText === correctAnswer2) {
       event.target.classList.add('bg-green-300');
       event.target.classList.remove('bg-slate-100');
@@ -87,8 +117,9 @@ const Option = ({ option, correctAnswer }) => {
         theme: "light",
       });
       notify();
-    } 
-
+      trueValue(1);
+    }
+    // implement false condition
     else {
       event.target.classList.add('bg-red-300');
       event.target.classList.remove('bg-slate-100');
@@ -103,19 +134,16 @@ const Option = ({ option, correctAnswer }) => {
         theme: "light",
       });
       notify();
+      falseValue(1);
     }
   }
 
   return (
-    <div className='my-2 rounded-md overflow-hidden'>
-      <button id='quiz-btn' onClick={handleButtonClick} className='text-left mx-4 md:mx-8 p-2 bg-slate-100 w-11/12 md:w-11/12 rounded-md'>
-        {option}
-      </button>
-    </div>
+    <button id='quiz-btn' onClick={handleButtonClick} className='my-1 text-left mx-4 md:mx-8 p-2 pl-4 bg-slate-100 w-11/12 md:w-11/12 rounded-lg'>
+      {option}
+    </button>
   );
 }
-
-
 
 
 export default Quiz;
